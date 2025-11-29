afa315ddbec781d116135ccdad0acc4bd0008678
atulsharma097 <officalatulsharma@gmail.com>
2025-11-29 11:21:21 +0530

    fix: normalize SkillScale filename and import case

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import LogoLoop from './components/LogoLoop';
 import { SiReact, SiNextdotjs, SiTailwindcss, SiMongodb, SiExpress, SiNodedotjs } from "react-icons/si";
 import ProjectSection from "./components/ProjectSection";
 import Card from './components/Card';
-import SkillScale from './components/skillScale';
+import SkillScale from './components/SkillScale';
 import Button from './components/Button';
 import AboutSection from './components/AboutSection';
 
